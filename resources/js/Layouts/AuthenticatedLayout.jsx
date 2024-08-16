546648a184e26a65ea132d85d76a9a726fd03d53
Francis Andoh <andohfrancis9187@gmail.com>
2024-08-16 15:38:19 +0000

    statup files

---
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import ApplicationLogo from '@/Components/ApplicationLogo';
+import Dropdown from '@/Components/Dropdown';
+import NavLink from '@/Components/NavLink';
+import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
+import { Link } from '@inertiajs/react';
+
+export default function Authenticated({ user, header, children }) {
+
+    return (
+        <div className="min-h-screen bg-background">
+       
+            <main>{children}</main>
+        </div>
+    );
+}

6cb1561606499a65289c346078d36bf59c973882
CobbyElsonfx <andohfrancis9187@gmail.com>
2024-11-22 06:30:43 +0000

    added new changes

--- resources/js/hooks/useLocalStorage.jsx
+++ resources/js/hooks/useLocalStorage.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 
 
 function useLocalStorage(
-  key){
+  key, initialValue){
   // State to store our value
   // Pass  initial state function to useState so logic is only executed once
   const [storedValue, setStoredValue] = useState(() => {
